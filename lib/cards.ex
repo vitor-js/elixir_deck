1a61bbd882f399a37fcfbbbed1bd517d5c6b096d
vitor-js <vitorgabriel9812@outlook.com>
2021-04-19 06:59:52 -0300

    feat: pipe function

--- lib/cards.ex
+++ lib/cards.ex
@@ -30,11 +30,16 @@ defmodule Cards do
 
   def load(filename) do
     {status, binary} = File.read filename
-
     case status do
       :ok -> :erlang.binary_to_term binary
       :error -> "That file does not exist"
     end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 
 end
